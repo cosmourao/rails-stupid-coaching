7973953b5bf96599769996da53b048d917d6ee24
Marcos Mourao <marcosfrimourao@gmail.com>
2021-10-05 21:23:15 -0300

    MVP working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,19 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    @answer = coach_logic(@question)
+  end
+
+  private
+
+  def coach_logic(message)
+    if message == 'I am going to work'
+      'Great!'
+    elsif message.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
